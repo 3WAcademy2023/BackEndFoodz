1ddb41596dd23bfd0022b3639961153ef0a4e49a
Khalilou11 <medkhalilterzi@gmail.com>
2023-09-22 10:31:06 +0200

    az

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import bodyparser from 'body-parser'
+import connectDB from './server.js'
+import cookieParser from 'cookie-parser';
+
+
+
+const app = express();
+const PORT = 9000;
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+//configuration de dossier views
+app.set("views", "views");
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+connectDB()
+app.use(cookieParser());
+app.use(express.static('public'))
+// app.use(ArticleRouter)
+// app.use(CategoryRouter)
+// app.use(CommentsRoute)
+// app.use(UserRouter)
